82efa79a17322f4e05a4a76b7e9bb357c3a978ab
ViggoOhlsson <viggo.e.g.o.@gmail.com>
2022-02-25 11:32:01 +0100

    index setup

---
+++ helpers.js
@@ -0,0 +1,8 @@
+const helpers = {
+    formatDate: (time) => {
+        const date = new Date(time)
+        return date.toLocaleDateString() + " " + date.toLocaleTimeString()
+    }
+}
+
+module.exports = helpers
---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+require("./mongoose.js")
+const express = require ("express")
+const exphbs = require("express-handlebars");
+const port = 80
+
+const app = express()
+app.engine("hbs", exphbs.engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: require("./helpers")
+}))
+app.set("view engine", "hbs")
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+
+app.use("/restaurants", require("./routes/restaurant-router"));
+app.use("/reviews", require("./routes/review-router"));
+
+app.get("/", (req,res) => {
+    res.send("Hello World!")
+})
+
+app.listen(port, () => {
+    console.log("http://localhost:" + port);
+})
