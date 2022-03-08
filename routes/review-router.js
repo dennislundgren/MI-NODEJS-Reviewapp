602f2a706a6cbaa120f4059dcaaaf2726e70ecd6
johannafryxell <johanna.fryxell@gmail.com>
2022-03-08 20:32:32 +0100

    Post edit review

--- routes/review-router.js
+++ routes/review-router.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const ReviewModel = require("../models/review.js");
-const RestaurantModel = require("../models/restaurant.js")
+const RestaurantModel = require("../models/restaurant.js");
 
 const router = express.Router();
 
 router.get("/write-new", async (req, res) => {
   if (res.locals.loggedIn) {
-    const restaurants = await RestaurantModel.find().lean()
+    const restaurants = await RestaurantModel.find().lean();
     console.log(restaurants);
-    res.render("review/write-new", {restaurants});
+    res.render("review/write-new", { restaurants });
   } else {
     res.redirect("/login");
   }
@@ -30,15 +30,34 @@ router.post("/write-new/:id", async (req, res) => {
 });
 
 router.get("/edit/:id", async (req, res, next) => {
-  const review = undefined;
+  //Kan jag göra populate om det finns flera reviews med samma restaurant id??
+  //Borde vara så??
+  //Hitta genom review id?
+  let review = undefined;
+  let restaurant = undefined;
   try {
-    review = await ReviewModel.findById(id).populate("restaurantId").lean();
+    review = await ReviewModel.findById(req.params.id).lean();
+    restaurant = await RestaurantModel.findById(review.restaurantId).lean();
+    // Jag har råkat skapa dummy-data som inte är riktiga object-id:n
+
+    console.log(review.restaurantId);
+    console.log(restaurant);
   } catch (error) {
     console.log(error.message);
     next(); //Fixa allmän error sida
   }
 
-  res.render("reviews/review-edit", review);
+  res.render("review/review-edit", { review, restaurant });
+});
+
+router.post("/edit/:id", async (rew, res, next) => {
+  review = await ReviewModel.findById(req.params.id).lean();
+
+  review.title = req.body.title;
+  review.description = req.body.description;
+  review.rating = req.body.rating;
+
+  await review.save();
 });
 
 module.exports = router;
