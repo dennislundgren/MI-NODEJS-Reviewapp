24cd01795a4d1c939145bf19224784d19738a524
dennislundgren <91125819+dennislundgren@users.noreply.github.com>
2022-03-08 11:46:37 +0100

    usermodels, locals id

--- routes/login-router.js
+++ routes/login-router.js
@@ -1,15 +1,11 @@
 //////////////
 // IMPORTS //
 ////////////
+require("../passport");
 const express = require("express");
 const router = express.Router();
 const cookieParser = require("cookie-parser");
-const {
-  UsersModel,
-  FacebookModel,
-  GoogleModel,
-  TwitterModel,
-} = require(".././models/UsersModel");
+const { UsersModel } = require(".././models/UsersModel");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 const { hashPassword, comparePassword } = require(".././utils");
@@ -35,7 +31,7 @@ router.post("/sign-in", async (req, res) => {
 
   UsersModel.findOne({ username }, (err, user) => {
     if (user && comparePassword(password, user.password)) {
-      const userData = { displayName: user.displayName };
+      const userData = { displayName: user.displayName, id: user._id };
       const accessToken = jwt.sign(userData, process.env.JWT_SECRET);
 
       if (rememberMe) {
@@ -58,6 +54,8 @@ router.post("/sign-up", async (req, res) => {
     if (user) {
       res.render("login", { signIn: false, userExists: true });
     } else if (password !== confirmPassword) {
+      res.render("login", { signIn: false, wrongPassword: true });
+    } else if (!password || !confirmPassword) {
       res.render("login", { signIn: false, wrongPassword: true });
     } else {
       const newUser = new UsersModel({
@@ -84,13 +82,17 @@ router.get(
     failureRedirect: "/",
   }),
   async (req, res) => {
-    GoogleModel.findOne({ googleId: req.user.id }, async (err, user) => {
+    console.log("Inne i callback.");
+    console.log(req.user);
+    UsersModel.findOne({ googleId: req.user.id }, async (err, user) => {
+      console.log("Försökt hitta användare i databasen.");
       const userData = { displayName: req.user.displayName };
 
       if (user) {
         userData.id = user._id;
       } else {
-        const newUser = new GoogleModel({
+        console.log("Försöker skapa ny användare i databasen.");
+        const newUser = new UsersModel({
           googleId: req.user.id,
           displayName: req.user.displayName,
         });
@@ -113,13 +115,16 @@ router.get(
   "/facebook/callback",
   passport.authenticate("facebook", { failureRedirect: "/" }),
   async (req, res) => {
-    FacebookModel.findOne({ facebookId: req.user.id }, async (err, user) => {
+    console.log("Inne i callback.");
+    UsersModel.findOne({ facebookId: req.user.id }, async (err, user) => {
+      console.log("Försökt hitta användare.");
       const userData = { displayName: req.user.displayName };
 
       if (user) {
         userData.id = user._id;
       } else {
-        const newUser = new FacebookModel({
+        console.log("Försöker skapa ny användare i databasen.");
+        const newUser = new UsersModel({
           facebookId: req.user.id,
           displayName: req.user.displayName,
         });
@@ -139,15 +144,15 @@ router.get(
   "/twitter/callback",
   passport.authenticate("twitter", { failureRedirect: "/" }),
   async (req, res) => {
-    TwitterModel.findOne({ twitterId: req.user.id }, async (err, user) => {
+    UsersModel.findOne({ twitterId: req.user.id }, async (err, user) => {
       const userData = { displayName: req.user.displayName };
 
       if (user) {
         userData.id = user._id;
       } else {
-        const newUser = new TwitterModel({
+        const newUser = new UsersModel({
           twitterId: req.user.id,
-          displayName: req.user.username,
+          displayName: req.user.displayName,
         });
         const result = await newUser.save();
         userData.id = result._id;
